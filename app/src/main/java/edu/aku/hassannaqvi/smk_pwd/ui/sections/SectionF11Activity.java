87d02917419dff17aa1d063eca93fae47395e0a8
Muhammad Hussain <ahsanpervez74@gmail.com>
2020-11-28 16:14:08 +0500

    Update SectionF11Activity

--- app/src/main/java/edu/aku/hassannaqvi/smk_pwd/ui/sections/SectionF11Activity.java
+++ app/src/main/java/edu/aku/hassannaqvi/smk_pwd/ui/sections/SectionF11Activity.java
@@ -1,13 +1,108 @@
 package edu.aku.hassannaqvi.smk_pwd.ui.sections;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.databinding.DataBindingUtil;
+import edu.aku.hassannaqvi.smk_pwd.R;
+import edu.aku.hassannaqvi.smk_pwd.contracts.FormsContract;
+import edu.aku.hassannaqvi.smk_pwd.core.DatabaseHelper;
+import edu.aku.hassannaqvi.smk_pwd.core.MainApp;
+import edu.aku.hassannaqvi.smk_pwd.databinding.ActivitySectionF11Binding;
+import edu.aku.hassannaqvi.smk_pwd.databinding.ActivitySectionF7Binding;
+import edu.aku.hassannaqvi.smk_pwd.utils.JSONUtils;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.widget.Toast;
+
+import com.validatorcrawler.aliazaz.Validator;
+
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import static edu.aku.hassannaqvi.smk_pwd.core.MainApp.fc;
+import static edu.aku.hassannaqvi.smk_pwd.utils.UtilKt.openSectionMainActivity;
 
 public class SectionF11Activity extends AppCompatActivity {
+
+    ActivitySectionF11Binding bi;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        bi = DataBindingUtil.setContentView(this, R.layout.activity_section_f11);
+        bi.setCallback(this);
+        setupSkips();
+    }
+
+    private void setupSkips() {
+    }
+
+
+    private boolean UpdateDB() {
+        DatabaseHelper db = MainApp.appInfo.getDbHelper();
+        int updcount = db.updatesFormColumn(FormsContract.FormsTable.COLUMN_SF, fc.getsF());
+        if (updcount == 1) {
+            return true;
+        } else {
+            Toast.makeText(this, "Sorry. You can't go further.\n Please contact IT Team (Failed to update DB)", Toast.LENGTH_SHORT).show();
+            return false;
+        }
+    }
+
+
+    private void SaveDraft() throws JSONException {
+
+        JSONObject json = new JSONObject();
+        json.put("fk01", bi.fk01a.isChecked() ? "1"
+                : bi.fk01b.isChecked() ? "2"
+                :  "-1");
+
+        json.put("fk02", bi.fk02a.isChecked() ? "1"
+                : bi.fk02b.isChecked() ? "2"
+                :  "-1");
+
+        json.put("fk03", bi.fk03a.isChecked() ? "1"
+                : bi.fk03b.isChecked() ? "2"
+                :  "-1");
+
+
+        try {
+            JSONObject json_merge = JSONUtils.mergeJSONObjects(new JSONObject(MainApp.fc.getsF()), json);
+
+            MainApp.fc.setsF(String.valueOf(json_merge));
+
+        } catch (JSONException e) {
+            e.printStackTrace();
+        }
+
+    }
+
+
+    private boolean formValidation() {
+        return Validator.emptyCheckingContainer(this, bi.GrpName);
+    }
+
+
+    public void BtnContinue() {
+        if (!formValidation()) return;
+        try {
+            SaveDraft();
+        } catch (JSONException e) {
+            e.printStackTrace();
+        }
+        if (UpdateDB()) {
+            finish();
+            startActivity(new Intent(this, SectionF12Activity.class));
+        }
+    }
+
+
+    public void BtnEnd() {
+        openSectionMainActivity(this, "F");
+    }
+
+    @Override
+    public void onBackPressed() {
+        Toast.makeText(this, "Back Press Not Allowed", Toast.LENGTH_SHORT).show();
     }
 }
