af38f5f92e244c97804f12f2c12c780b7325c8bb
Muhammad Hussain <ahsanpervez74@gmail.com>
2020-11-28 16:19:30 +0500

    Update SectionF13Activity

--- app/src/main/java/edu/aku/hassannaqvi/smk_pwd/ui/sections/SectionF13Activity.java
+++ app/src/main/java/edu/aku/hassannaqvi/smk_pwd/ui/sections/SectionF13Activity.java
@@ -1,13 +1,127 @@
 package edu.aku.hassannaqvi.smk_pwd.ui.sections;
 
-import androidx.appcompat.app.AppCompatActivity;
-
+import android.content.Intent;
 import android.os.Bundle;
+import android.widget.Toast;
+
+import com.validatorcrawler.aliazaz.Validator;
+
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.databinding.DataBindingUtil;
+import edu.aku.hassannaqvi.smk_pwd.R;
+import edu.aku.hassannaqvi.smk_pwd.contracts.FormsContract;
+import edu.aku.hassannaqvi.smk_pwd.core.DatabaseHelper;
+import edu.aku.hassannaqvi.smk_pwd.core.MainApp;
+import edu.aku.hassannaqvi.smk_pwd.databinding.ActivitySectionF13Binding;
+import edu.aku.hassannaqvi.smk_pwd.utils.JSONUtils;
+
+import static edu.aku.hassannaqvi.smk_pwd.core.MainApp.fc;
+import static edu.aku.hassannaqvi.smk_pwd.utils.UtilKt.openSectionMainActivity;
 
 public class SectionF13Activity extends AppCompatActivity {
+
+    ActivitySectionF13Binding bi;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        bi = DataBindingUtil.setContentView(this, R.layout.activity_section_f13);
+        bi.setCallback(this);
+        setupSkips();
+    }
+
+    private void setupSkips() {
+    }
+
+
+    private boolean UpdateDB() {
+        DatabaseHelper db = MainApp.appInfo.getDbHelper();
+        int updcount = db.updatesFormColumn(FormsContract.FormsTable.COLUMN_SF, fc.getsF());
+        if (updcount == 1) {
+            return true;
+        } else {
+            Toast.makeText(this, "Sorry. You can't go further.\n Please contact IT Team (Failed to update DB)", Toast.LENGTH_SHORT).show();
+            return false;
+        }
+    }
+
+
+    private void SaveDraft() throws JSONException {
+
+        JSONObject json = new JSONObject();
+        json.put("fm01", bi.fm01a.isChecked() ? "1"
+                : bi.fm01b.isChecked() ? "2"
+                : "-1");
+
+        json.put("fm02", bi.fm02a.isChecked() ? "1"
+                : bi.fm02b.isChecked() ? "2"
+                : "-1");
+
+        json.put("fm03", bi.fm03a.isChecked() ? "1"
+                : bi.fm03b.isChecked() ? "2"
+                : "-1");
+
+        json.put("fm04", bi.fm04a.isChecked() ? "1"
+                : bi.fm04b.isChecked() ? "2"
+                : "-1");
+
+        json.put("fm05", bi.fm05a.isChecked() ? "1"
+                : bi.fm05b.isChecked() ? "2"
+                : "-1");
+
+        json.put("fm06", bi.fm06a.isChecked() ? "1"
+                : bi.fm06b.isChecked() ? "2"
+                : "-1");
+
+        json.put("fm07", bi.fm07a.isChecked() ? "1"
+                : bi.fm07b.isChecked() ? "2"
+                : "-1");
+
+        json.put("fm08", bi.fm08a.isChecked() ? "1"
+                : bi.fm08b.isChecked() ? "2"
+                : "-1");
+
+
+        try {
+            JSONObject json_merge = JSONUtils.mergeJSONObjects(new JSONObject(MainApp.fc.getsF()), json);
+
+            MainApp.fc.setsF(String.valueOf(json_merge));
+
+        } catch (JSONException e) {
+            e.printStackTrace();
+        }
+
+    }
+
+
+    private boolean formValidation() {
+        return Validator.emptyCheckingContainer(this, bi.GrpName);
+    }
+
+
+    public void BtnContinue() {
+        if (!formValidation()) return;
+        try {
+            SaveDraft();
+        } catch (JSONException e) {
+            e.printStackTrace();
+        }
+        if (UpdateDB()) {
+            finish();
+            startActivity(new Intent(this, SectionF14Activity.class));
+        }
+    }
+
+
+    public void BtnEnd() {
+        openSectionMainActivity(this, "F");
+    }
+
+    @Override
+    public void onBackPressed() {
+        Toast.makeText(this, "Back Press Not Allowed", Toast.LENGTH_SHORT).show();
     }
 }
