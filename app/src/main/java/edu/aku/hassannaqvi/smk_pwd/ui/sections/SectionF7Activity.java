e3accd47178fc5f68c95bcecc3f26e75cfd97bca
SAJID LATIF <indus1530@gmail.com>
2020-12-19 12:11:58 +0500

    SectionF7Activity skip&saveDraft

--- app/src/main/java/edu/aku/hassannaqvi/smk_pwd/ui/sections/SectionF7Activity.java
+++ app/src/main/java/edu/aku/hassannaqvi/smk_pwd/ui/sections/SectionF7Activity.java
@@ -15,11 +15,14 @@ import com.validatorcrawler.aliazaz.Clear;
 import com.validatorcrawler.aliazaz.Validator;
 
 import org.jetbrains.annotations.NotNull;
-import org.json.JSONException;
 
 import edu.aku.hassannaqvi.smk_pwd.R;
+import edu.aku.hassannaqvi.smk_pwd.contracts.FormsContract;
+import edu.aku.hassannaqvi.smk_pwd.core.DatabaseHelper;
+import edu.aku.hassannaqvi.smk_pwd.core.MainApp;
 import edu.aku.hassannaqvi.smk_pwd.databinding.ActivitySectionF7Binding;
 
+import static edu.aku.hassannaqvi.smk_pwd.core.MainApp.form;
 import static edu.aku.hassannaqvi.smk_pwd.utils.UtilKt.openSectionMainActivity;
 
 
@@ -34,6 +37,7 @@ public class SectionF7Activity extends AppCompatActivity {
         bi.setCallback(this);
         setupSkips();
     }
+
 
     private void setupSkips() {
         radioGroupListener(bi.fg02, bi.fg02b, bi.cvfg03);
@@ -52,42 +56,40 @@ public class SectionF7Activity extends AppCompatActivity {
 
 
     private boolean UpdateDB() {
-        /*DatabaseHelper db = MainApp.appInfo.getDbHelper();
-        int updcount = db.updatesFormColumn(FormsContract.FormsTable.COLUMN_SF, fc.getsF());
+        DatabaseHelper db = MainApp.appInfo.getDbHelper();
+        int updcount = db.updatesFormColumn(FormsContract.FormsTable.COLUMN_SF, form.getsF());
         if (updcount == 1) {
             return true;
         } else {
-            Toast.makeText(this, "Sorry. You can't go further.\n Please contact IT Team (Failed to update DB)", Toast.LENGTH_SHORT).show();
+            Toast.makeText(this, "SORRY! Failed to update DB", Toast.LENGTH_SHORT).show();
             return false;
-        }*/
-        return true;
+        }
     }
 
 
-    private void SaveDraft() throws JSONException {
+    private void SaveDraft() {
 
-       /* JSONObject json = new JSONObject();
-        json.put("fg01", bi.fg01a.isChecked() ? "1"
+        form.setFg01(bi.fg01a.isChecked() ? "1"
                 : bi.fg01b.isChecked() ? "2"
                 : "-1");
 
-        json.put("fg02", bi.fg02a.isChecked() ? "1"
+        form.setFg02(bi.fg02a.isChecked() ? "1"
                 : bi.fg02b.isChecked() ? "2"
                 : "-1");
 
-        json.put("fg03", bi.fg03a.isChecked() ? "1"
+        form.setFg03(bi.fg03a.isChecked() ? "1"
                 : bi.fg03b.isChecked() ? "2"
                 : "-1");
 
-        json.put("fg04", bi.fg04a.isChecked() ? "1"
+        form.setFg04(bi.fg04a.isChecked() ? "1"
                 : bi.fg04b.isChecked() ? "2"
                 : "-1");
 
-        json.put("fg05", bi.fg05a.isChecked() ? "1"
+        form.setFg05(bi.fg05a.isChecked() ? "1"
                 : bi.fg05b.isChecked() ? "2"
                 : "-1");
 
-        try {
+        /*try {
             JSONObject json_merge = JSONUtils.mergeJSONObjects(new JSONObject(MainApp.fc.getsF()), json);
 
             MainApp.fc.setsF(String.valueOf(json_merge));
@@ -99,18 +101,16 @@ public class SectionF7Activity extends AppCompatActivity {
     }
 
 
+
     private boolean formValidation() {
         return Validator.emptyCheckingContainer(this, bi.GrpName);
     }
 
 
+
     public void BtnContinue() {
         if (!formValidation()) return;
-        try {
-            SaveDraft();
-        } catch (JSONException e) {
-            e.printStackTrace();
-        }
+        SaveDraft();
         if (UpdateDB()) {
             finish();
             startActivity(new Intent(this, SectionF8Activity.class));
@@ -121,6 +121,7 @@ public class SectionF7Activity extends AppCompatActivity {
     public void BtnEnd() {
         openSectionMainActivity(this, "F");
     }
+
 
     @Override
     public void onBackPressed() {
