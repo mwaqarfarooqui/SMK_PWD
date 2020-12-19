086e74978dfbd5b6a6b4ddb4ab9142e9dd892126
SAJID LATIF <indus1530@gmail.com>
2020-12-19 12:38:39 +0500

    SectionF16Activity skip&saveDraft

--- app/src/main/java/edu/aku/hassannaqvi/smk_pwd/ui/sections/SectionF16Activity.java
+++ app/src/main/java/edu/aku/hassannaqvi/smk_pwd/ui/sections/SectionF16Activity.java
@@ -15,12 +15,15 @@ import com.validatorcrawler.aliazaz.Clear;
 import com.validatorcrawler.aliazaz.Validator;
 
 import org.jetbrains.annotations.NotNull;
-import org.json.JSONException;
 
 import edu.aku.hassannaqvi.smk_pwd.R;
+import edu.aku.hassannaqvi.smk_pwd.contracts.FormsContract;
+import edu.aku.hassannaqvi.smk_pwd.core.DatabaseHelper;
+import edu.aku.hassannaqvi.smk_pwd.core.MainApp;
 import edu.aku.hassannaqvi.smk_pwd.databinding.ActivitySectionF16Binding;
 import edu.aku.hassannaqvi.smk_pwd.ui.other.SectionMainActivity;
 
+import static edu.aku.hassannaqvi.smk_pwd.core.MainApp.form;
 import static edu.aku.hassannaqvi.smk_pwd.utils.UtilKt.openSectionMainActivity;
 
 public class SectionF16Activity extends AppCompatActivity {
@@ -61,103 +64,92 @@ public class SectionF16Activity extends AppCompatActivity {
 
 
     private boolean UpdateDB() {
-       /* DatabaseHelper db = MainApp.appInfo.getDbHelper();
-        int updcount = db.updatesFormColumn(FormsContract.FormsTable.COLUMN_SF, fc.getsF());
+        DatabaseHelper db = MainApp.appInfo.getDbHelper();
+        int updcount = db.updatesFormColumn(FormsContract.FormsTable.COLUMN_SF, form.getsF());
         if (updcount == 1) {
             return true;
         } else {
-            Toast.makeText(this, "Sorry. You can't go further.\n Please contact IT Team (Failed to update DB)", Toast.LENGTH_SHORT).show();
+            Toast.makeText(this, "SORRY! Failed to update DB", Toast.LENGTH_SHORT).show();
             return false;
-        }*/
-        return true;
+        }
     }
 
 
-    private void SaveDraft() throws JSONException {
+    private void SaveDraft() {
 
-        /*JSONObject json = new JSONObject();
-        json.put("fp01a", bi.fp01aa.isChecked() ? "1"
+        form.setFp01a(bi.fp01aa.isChecked() ? "1"
                 : bi.fp01ab.isChecked() ? "2"
                 : "-1");
 
-        json.put("fp01b", bi.fp01ba.isChecked() ? "1"
+        form.setFp01b(bi.fp01ba.isChecked() ? "1"
                 : bi.fp01bb.isChecked() ? "2"
                 : "-1");
 
-        json.put("fp01c", bi.fp01ca.isChecked() ? "1"
+        form.setFp01c(bi.fp01ca.isChecked() ? "1"
                 : bi.fp01cb.isChecked() ? "2"
                 : "-1");
 
-        json.put("fp01dd", bi.fp01dd.getText().toString());
-
-        json.put("fp01dm", bi.fp01dm.getText().toString());
-
-        json.put("fp02a", bi.fp02aa.isChecked() ? "1"
+        form.setFp01dd(bi.fp01dd.getText().toString());
+        form.setFp01dm(bi.fp01dm.getText().toString());
+        form.setFp02a(bi.fp02aa.isChecked() ? "1"
                 : bi.fp02ab.isChecked() ? "2"
                 : "-1");
 
-        json.put("fp02b", bi.fp02ba.isChecked() ? "1"
+        form.setFp02b(bi.fp02ba.isChecked() ? "1"
                 : bi.fp02bb.isChecked() ? "2"
                 : "-1");
 
-        json.put("fp02c", bi.fp02ca.isChecked() ? "1"
+        form.setFp02c(bi.fp02ca.isChecked() ? "1"
                 : bi.fp02cb.isChecked() ? "2"
                 : "-1");
 
-        json.put("fp02dd", bi.fp02dd.getText().toString());
-
-        json.put("fp02dm", bi.fp02dm.getText().toString());
-
-        json.put("fp03a", bi.fp03aa.isChecked() ? "1"
+        form.setFp02dd(bi.fp02dd.getText().toString());
+        form.setFp02dm(bi.fp02dm.getText().toString());
+        form.setFp03a(bi.fp03aa.isChecked() ? "1"
                 : bi.fp03ab.isChecked() ? "2"
                 : "-1");
 
-        json.put("fp03b", bi.fp03ba.isChecked() ? "1"
+        form.setFp03b(bi.fp03ba.isChecked() ? "1"
                 : bi.fp03bb.isChecked() ? "2"
                 : "-1");
 
-        json.put("fp03c", bi.fp03ca.isChecked() ? "1"
+        form.setFp03c(bi.fp03ca.isChecked() ? "1"
                 : bi.fp03cb.isChecked() ? "2"
                 : "-1");
 
-        json.put("fp03dd", bi.fp03dd.getText().toString());
-
-        json.put("fp03dm", bi.fp03dm.getText().toString());
-
-        json.put("fp04a", bi.fp04aa.isChecked() ? "1"
+        form.setFp03dd(bi.fp03dd.getText().toString());
+        form.setFp03dm(bi.fp03dm.getText().toString());
+        form.setFp04a(bi.fp04aa.isChecked() ? "1"
                 : bi.fp04ab.isChecked() ? "2"
                 : "-1");
 
-        json.put("fp04b", bi.fp04ba.isChecked() ? "1"
+        form.setFp04b(bi.fp04ba.isChecked() ? "1"
                 : bi.fp04bb.isChecked() ? "2"
                 : "-1");
 
-        json.put("fp04c", bi.fp04ca.isChecked() ? "1"
+        form.setFp04c(bi.fp04ca.isChecked() ? "1"
                 : bi.fp04cb.isChecked() ? "2"
                 : "-1");
 
-        json.put("fp04dd", bi.fp04dd.getText().toString());
-
-        json.put("fp04dm", bi.fp04dm.getText().toString());
-
-        json.put("fp05a", bi.fp05aa.isChecked() ? "1"
+        form.setFp04dd(bi.fp04dd.getText().toString());
+        form.setFp04dm(bi.fp04dm.getText().toString());
+        form.setFp05a(bi.fp05aa.isChecked() ? "1"
                 : bi.fp05ab.isChecked() ? "2"
                 : "-1");
 
-        json.put("fp05b", bi.fp05ba.isChecked() ? "1"
+        form.setFp05b(bi.fp05ba.isChecked() ? "1"
                 : bi.fp05bb.isChecked() ? "2"
                 : "-1");
 
-        json.put("fp05c", bi.fp05ca.isChecked() ? "1"
+        form.setFp05c(bi.fp05ca.isChecked() ? "1"
                 : bi.fp05cb.isChecked() ? "2"
                 : "-1");
 
-        json.put("fp05dd", bi.fp05dd.getText().toString());
+        form.setFp05dd(bi.fp05dd.getText().toString());
+        form.setFp05dm(bi.fp05dm.getText().toString());
 
-        json.put("fp05dm", bi.fp05dm.getText().toString());
 
-
-        try {
+        /*try {
             JSONObject json_merge = JSONUtils.mergeJSONObjects(new JSONObject(MainApp.fc.getsF()), json);
 
             MainApp.fc.setsF(String.valueOf(json_merge));
@@ -176,11 +168,7 @@ public class SectionF16Activity extends AppCompatActivity {
 
     public void BtnContinue() {
         if (!formValidation()) return;
-        try {
-            SaveDraft();
-        } catch (JSONException e) {
-            e.printStackTrace();
-        }
+        SaveDraft();
         if (UpdateDB()) {
             finish();
             startActivity(new Intent(this, SectionMainActivity.class));
@@ -192,8 +180,11 @@ public class SectionF16Activity extends AppCompatActivity {
         openSectionMainActivity(this, "F");
     }
 
+
     @Override
     public void onBackPressed() {
         Toast.makeText(this, "Back Press Not Allowed", Toast.LENGTH_SHORT).show();
     }
+
+
 }
